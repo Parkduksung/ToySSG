8d0d5b9e071307c1e65ade7a87b0acc725f3e348
Parkduksung <duksung1234@naver.com>
2021-10-30 18:05:08 +0900

    [feature] RED, ssgRemoteDataSource, ssgRepositoryImpl 추가.

--- app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
+++ app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
@@ -1,13 +1,35 @@
 package com.example.toyssg.data.repo
 
+import com.example.toyssg.data.source.remote.SSGRemoteDataSource
+import com.example.toyssg.data.source.remote.SSGRemoteDataSourceImpl
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class SSGRepositoryImplTest {
+
+    @Mock
+    lateinit var ssgRemoteDataSource: SSGRemoteDataSource
+
+
+    private lateinit var ssgRepositoryImpl: SSGRepositoryImpl
+
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        ssgRemoteDataSource = Mockito.mock(SSGRemoteDataSourceImpl::class.java)
+        ssgRepositoryImpl = SSGRepositoryImpl(ssgRemoteDataSource)
+    }
 
     @Test
     fun checkGetSSGItemResponseSuccessTest() = runBlocking {
