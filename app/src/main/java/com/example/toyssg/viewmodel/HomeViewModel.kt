c47969e2f7cc6cbc3a5cd164c577393d6247d1f1
Parkduksung <duksung1234@naver.com>
2021-10-30 19:30:14 +0900

    [feature] REFACTOR, BaseViewModel 적용.

--- app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
@@ -1,36 +1,30 @@
 package com.example.toyssg.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
 import com.example.toyssg.api.response.SSGData
+import com.example.toyssg.base.BaseViewModel
+import com.example.toyssg.base.ViewState
 import com.example.toyssg.data.repo.SSGRepository
+import com.example.toyssg.ext.ioScope
 import com.example.toyssg.util.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val ssgRepository: SSGRepository) : ViewModel() {
+class HomeViewModel @Inject constructor(
+    app: Application,
+    private val ssgRepository: SSGRepository
+) : BaseViewModel(app) {
 
+    fun getSSGItemResponse() {
+        ioScope {
+            when (val result = ssgRepository.getSSGItemResponse()) {
 
-    private val _viewStateLiveData = MutableLiveData<ViewState>()
-    val viewStateLiveData: LiveData<ViewState> = _viewStateLiveData
-
-
-    suspend fun getSSGItemResponse() {
-        when (val result = ssgRepository.getSSGItemResponse()) {
-
-            is Result.Success -> {
-                withContext(Dispatchers.Main) {
-                    _viewStateLiveData.value = HomeViewState.GetSSGItemList(result.data.data)
+                is Result.Success -> {
+                    viewStateChanged(HomeViewState.GetSSGItemList(result.data.data))
                 }
-            }
-            is Result.Error -> {
-                withContext(Dispatchers.Main) {
-                    _viewStateLiveData.value =
-                        HomeViewState.Error(result.exception.message.toString())
+                is Result.Error -> {
+                    viewStateChanged(HomeViewState.Error(result.exception.message.toString()))
                 }
             }
         }
@@ -43,5 +37,3 @@ class HomeViewModel @Inject constructor(private val ssgRepository: SSGRepository
     }
 
 }
-
-interface ViewState
