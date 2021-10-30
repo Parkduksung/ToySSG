7ef6b50ac87d01864cbd327a4a6e712eebb069df
Parkduksung <duksung1234@naver.com>
2021-10-30 18:46:32 +0900

    [feature] RED, HomeViewModel TestCode 추가.

--- app/src/test/java/com/example/toyssg/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/com/example/toyssg/viewmodel/HomeViewModelTest.kt
@@ -1,29 +1,63 @@
 package com.example.toyssg.viewmodel
 
+import androidx.lifecycle.Observer
 import base.BaseTest
 import com.example.toyssg.data.repo.SSGRepository
 import com.example.toyssg.data.repo.SSGRepositoryImpl
+import com.example.toyssg.data.source.remote.SSGRemoteDataSourceImplTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
+import com.example.toyssg.util.Result
 
 class HomeViewModelTest : BaseTest() {
-
 
     @Mock
     lateinit var ssgRepository: SSGRepository
 
     private lateinit var homeViewModel: HomeViewModel
 
+    @Mock
+    lateinit var viewStateObserver: Observer<ViewState>
 
     @ExperimentalCoroutinesApi
     override fun setup() {
         super.setup()
         ssgRepository = Mockito.mock(SSGRepositoryImpl::class.java)
         homeViewModel = HomeViewModel(ssgRepository)
+        homeViewModel.viewStateLiveData.observeForever(viewStateObserver)
     }
 
+    @Test
+    fun checkGetSSGItemResponseSuccessTest() = runBlocking {
 
+        val successResult =
+            Result.Success(SSGRemoteDataSourceImplTest.mockSSGItemResponse(mockResult = "200"))
+
+        Mockito.`when`(ssgRepository.getSSGItemResponse()).thenReturn(successResult)
+
+        homeViewModel.getSSGItemResponse()
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.GetSSGItemList(successResult.data.data))
+
+    }
+
+    @Test
+    fun checkGetSSGItemResponseFailTest() = runBlocking {
+
+        val failResult = Result.Error(Exception("Error GetSSGItemResponse!"))
+
+        Mockito.`when`(ssgRepository.getSSGItemResponse()).thenReturn(failResult)
+
+        homeViewModel.getSSGItemResponse()
+
+        Mockito.verify(viewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.Error(failResult.exception.message.toString()))
+
+    }
 
 }
