7d3fd07924e6c61376285bc42ada90c0d5b94a27
Parkduksung <duksung1234@naver.com>
2021-10-30 19:20:44 +0900

    [feature] HomeActivity 에 HomeViewModel 생성.

--- app/src/main/java/com/example/toyssg/ui/HomeActivity.kt
+++ app/src/main/java/com/example/toyssg/ui/HomeActivity.kt
@@ -1,10 +1,39 @@
 package com.example.toyssg.ui
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.toyssg.R
+import com.example.toyssg.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeActivity : AppCompatActivity(R.layout.home_act) {
 
+    private val homeViewModel by viewModels<HomeViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initViewModel()
+
+    }
+
+    private fun initViewModel() {
+        homeViewModel.viewStateLiveData.observe(this) { viewState ->
+            (viewState as? HomeViewModel.HomeViewState)?.let { onChangedHomeViewState(viewState) }
+        }
+    }
+
+    private fun onChangedHomeViewState(viewState: HomeViewModel.HomeViewState) {
+        when(viewState) {
+            is HomeViewModel.HomeViewState.GetSSGItemList -> {
+
+            }
+
+            is HomeViewModel.HomeViewState.Error -> {
+
+            }
+        }
+    }
 }
