33a8f4c55f469195c43f88cf03a49642170aef5d
Parkduksung <duksung1234@naver.com>
2021-10-30 16:56:09 +0900

    [feature] RED, setup

--- app/src/test/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImplTest.kt
+++ app/src/test/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImplTest.kt
@@ -1,7 +1,14 @@
 package com.example.toyssg.data.source.remote
 
+import com.example.toyssg.api.SSGApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 
@@ -9,6 +16,18 @@ import org.mockito.junit.MockitoJUnitRunner
 class SSGRemoteDataSourceImplTest {
 
 
+    @Mock
+    lateinit var ssgApi: SSGApi
+
+
+    private lateinit var ssgRemoteDataSourceImpl: SSGRemoteDataSourceImpl
+
+    @Before
+    fun setup() {
+        Dispatchers.setMain(TestCoroutineDispatcher())
+        ssgApi = Mockito.mock(SSGApi::class.java)
+        ssgRemoteDataSourceImpl = SSGRemoteDataSourceImpl(ssgApi)
+    }
 
     @Test
     fun checkGetSSGItemSuccessTest() {
