57cceebd94bf5fbea74ebbe57069ae4766bef692
Parkduksung <duksung1234@naver.com>
2021-10-30 17:16:58 +0900

    [feature] RED, initMockSSGApi 생성.

--- app/src/test/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImplTest.kt
+++ app/src/test/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImplTest.kt
@@ -4,12 +4,17 @@ import com.example.toyssg.api.SSGApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
+import okhttp3.Request
+import okio.Timeout
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 @RunWith(MockitoJUnitRunner::class)
@@ -39,4 +44,42 @@ class SSGRemoteDataSourceImplTest {
 
     }
 
+    fun initMockSSGApi(response: SSGItemResponse) {
+
+        Mockito.`when`(ssgApi.getSSGItem()).thenReturn(
+            object : Call<SSGItemResponse> {
+                override fun clone(): Call<SSGItemResponse> {
+                    TODO("Not yet implemented")
+                }
+
+                override fun execute(): Response<SSGItemResponse> {
+                    return Response.success(response)
+                }
+
+                override fun enqueue(callback: Callback<SSGItemResponse>) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun isExecuted(): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                override fun cancel() {
+                    TODO("Not yet implemented")
+                }
+
+                override fun isCanceled(): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                override fun request(): Request {
+                    TODO("Not yet implemented")
+                }
+
+                override fun timeout(): Timeout {
+                    TODO("Not yet implemented")
+                }
+            }
+        )
+    }
 }
