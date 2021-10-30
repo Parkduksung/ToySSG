6726a0e5320a437aca6fbe7143122a31cfb05dbe
Parkduksung <duksung1234@naver.com>
2021-10-30 18:03:15 +0900

    [feature] SSGRepositoryImplTest TestCase 생성.

--- app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
+++ app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
@@ -1,5 +1,22 @@
 package com.example.toyssg.data.repo
 
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
 
-class SSGRepositoryImplTest
+@RunWith(MockitoJUnitRunner::class)
+class SSGRepositoryImplTest {
+
+    @Test
+    fun checkGetSSGItemResponseSuccessTest() = runBlocking {
+
+    }
+
+    @Test
+    fun checkGetSSGItemResponseFailTest() = runBlocking {
+
+    }
+
+}
