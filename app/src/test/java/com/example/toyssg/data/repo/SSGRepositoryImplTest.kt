27a7782c3497e4e1a3b3029c8cee720331bbb4cf
Parkduksung <duksung1234@naver.com>
2021-10-30 18:17:01 +0900

    [feature] RED, 테스트 코드 추가.

--- app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
+++ app/src/test/java/com/example/toyssg/data/repo/SSGRepositoryImplTest.kt
@@ -1,11 +1,15 @@
 package com.example.toyssg.data.repo
 
+import com.example.toyssg.api.response.SSGItemResponse
 import com.example.toyssg.data.source.remote.SSGRemoteDataSource
 import com.example.toyssg.data.source.remote.SSGRemoteDataSourceImpl
+import com.example.toyssg.data.source.remote.SSGRemoteDataSourceImplTest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -13,6 +17,8 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
+import java.lang.Exception
+import com.example.toyssg.util.Result
 
 @RunWith(MockitoJUnitRunner::class)
 class SSGRepositoryImplTest {
@@ -34,10 +40,35 @@ class SSGRepositoryImplTest {
     @Test
     fun checkGetSSGItemResponseSuccessTest() = runBlocking {
 
+        val successResult =
+            Result.Success(SSGRemoteDataSourceImplTest.mockSSGItemResponse(mockResult = "200"))
+
+
+        Mockito.`when`(ssgRemoteDataSource.getSSGItemResponse())
+            .thenReturn(successResult)
+
+        MatcherAssert.assertThat(
+            "올바른 SSGItemResponse 값이 전달받았으므로 성공",
+            ((ssgRepositoryImpl.getSSGItemResponse() as Result.Success<SSGItemResponse>).data.result,
+            Matchers.`is`(successResult.data.result)
+        )
+
+
     }
 
     @Test
     fun checkGetSSGItemResponseFailTest() = runBlocking {
+
+        val failResult = Result.Error(Exception("Error GetSSGItemResponse!"))
+
+        Mockito.`when`(ssgRemoteDataSource.getSSGItemResponse())
+            .then { failResult }
+
+        MatcherAssert.assertThat(
+            "Error 가 발생했으므로 실패.",
+            ((ssgRepositoryImpl.getSSGItemResponse() as Result.Error).exception.message),
+            Matchers.`is`(failResult.exception.message)
+        )
 
     }
 
