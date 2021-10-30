be462e0912149893c2ac676b0c59944b95579b3c
Parkduksung <duksung1234@naver.com>
2021-10-30 19:28:24 +0900

    [feature] BaseViewModel, ViewModelExt 생성.

---
+++ app/src/main/java/com/example/toyssg/base/BaseViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.toyssg.base
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.toyssg.ext.uiScope
+
+abstract class BaseViewModel(app: Application) : AndroidViewModel(app) {
+
+    private val _viewStateLiveData = MutableLiveData<ViewState>()
+    val viewStateLiveData: LiveData<ViewState> = _viewStateLiveData
+
+    protected fun viewStateChanged(viewState: ViewState) {
+        uiScope {
+            _viewStateLiveData.value = viewState
+            _viewStateLiveData.value = null
+        }
+    }
+
+}
+
+interface ViewState
