edfae5319333e608a470042c3e402ed3ef19e089
Parkduksung <duksung1234@naver.com>
2021-10-30 18:52:56 +0900

    [feature] Green, 테스트 코드 통과. HomeViewModel Coverage 100.

--- app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
@@ -1,8 +1,44 @@
 package com.example.toyssg.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.toyssg.api.response.SSGData
 import com.example.toyssg.data.repo.SSGRepository
+import com.example.toyssg.util.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class HomeViewModel(private val ssgRepository: SSGRepository) : ViewModel() {
 
+
+    private val _viewStateLiveData = MutableLiveData<ViewState>()
+    val viewStateLiveData: LiveData<ViewState> = _viewStateLiveData
+
+
+    suspend fun getSSGItemResponse() {
+        when (val result = ssgRepository.getSSGItemResponse()) {
+
+            is Result.Success -> {
+                withContext(Dispatchers.Main) {
+                    _viewStateLiveData.value = HomeViewState.GetSSGItemList(result.data.data)
+                }
+            }
+            is Result.Error -> {
+                withContext(Dispatchers.Main) {
+                    _viewStateLiveData.value =
+                        HomeViewState.Error(result.exception.message.toString())
+                }
+            }
+        }
+    }
+
+
+    sealed class HomeViewState : ViewState {
+        data class GetSSGItemList(val list: List<SSGData>) : HomeViewState()
+        data class Error(val message: String) : HomeViewState()
+    }
+
 }
+
+interface ViewState
