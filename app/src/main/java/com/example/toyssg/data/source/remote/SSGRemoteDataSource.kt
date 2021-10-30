2c18d66c5e47a274a7625b3ddb92571d748c18c0
Parkduksung <duksung1234@naver.com>
2021-10-30 17:43:32 +0900

    [feature] REFACTOR, interface 로 getSSGItemResponse 이동. suspend 추가.

--- app/src/main/java/com/example/toyssg/data/source/remote/SSGRemoteDataSource.kt
+++ app/src/main/java/com/example/toyssg/data/source/remote/SSGRemoteDataSource.kt
@@ -1,4 +1,7 @@
 package com.example.toyssg.data.source.remote
 
+import com.example.toyssg.api.response.SSGItemResponse
+
 interface SSGRemoteDataSource {
+    suspend fun getSSGItemResponse(): Result<SSGItemResponse>
 }
--- app/src/main/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/toyssg/data/source/remote/SSGRemoteDataSourceImpl.kt
@@ -2,9 +2,12 @@ package com.example.toyssg.data.source.remote
 
 import com.example.toyssg.api.SSGApi
 import com.example.toyssg.api.response.SSGItemResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class SSGRemoteDataSourceImpl(private val ssgApi: SSGApi) : SSGRemoteDataSource {
-    fun getSSGItemResponse(): Result<SSGItemResponse> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getSSGItemResponse(): Result<SSGItemResponse> =
+        withContext(Dispatchers.IO) {
+            TODO("Not yet implemented")
+        }
 }
