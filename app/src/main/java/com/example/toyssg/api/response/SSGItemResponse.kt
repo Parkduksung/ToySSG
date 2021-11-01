b46e1289477ca13f47fef8757ca37a9c4f4d7bce
Parkduksung <duksung1234@naver.com>
2021-11-01 18:12:03 +0900

    [feature] Parcelize 적용.

--- app/src/main/java/com/example/toyssg/api/response/SSGItemResponse.kt
+++ app/src/main/java/com/example/toyssg/api/response/SSGItemResponse.kt
@@ -1,6 +1,9 @@
 package com.example.toyssg.api.response
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
 
 data class SSGItemResponse(
     @SerializedName("data") val dataList: List<SSGData>,
@@ -13,9 +16,11 @@ data class SSGData(
     @SerializedName("viewtype") val viewType: String
 )
 
+@Parcelize
 data class SSGItem(
     @SerializedName("detail") var detail: String? = null,
     @SerializedName("image") var image: String? = null,
     @SerializedName("name") var name: String? = null,
     @SerializedName("price") var price: String? = null
-)
+) : Parcelable
+
