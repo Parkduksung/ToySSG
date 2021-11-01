d81901db31b3fa037e4437094ed0dae06cc4ba73
Parkduksung <duksung1234@naver.com>
2021-11-01 18:15:13 +0900

    [feature] HomeActivity, HomeViewModel 수정.

--- app/src/main/java/com/example/toyssg/ui/HomeActivity.kt
+++ app/src/main/java/com/example/toyssg/ui/HomeActivity.kt
@@ -1,11 +1,12 @@
 package com.example.toyssg.ui
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
 import com.example.toyssg.R
 import com.example.toyssg.base.BaseActivity
 import com.example.toyssg.databinding.HomeActBinding
-import com.example.toyssg.ui.adapter.SSGAdapter
 import com.example.toyssg.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,38 +15,53 @@ class HomeActivity : BaseActivity<HomeActBinding>(R.layout.home_act) {
 
     private val homeViewModel by viewModels<HomeViewModel>()
 
-    private val ssgAdapter by lazy { SSGAdapter() }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        initUi()
         initViewModel()
 
-    }
-
-    private fun initUi() {
-        binding.rvSsg.apply {
-            adapter = ssgAdapter
-        }
     }
 
     private fun initViewModel() {
         homeViewModel.viewStateLiveData.observe(this) { viewState ->
             (viewState as? HomeViewModel.HomeViewState)?.let { onChangedHomeViewState(viewState) }
         }
-
-        homeViewModel.getSSGItemResponse()
     }
 
     private fun onChangedHomeViewState(viewState: HomeViewModel.HomeViewState) {
         when (viewState) {
-            is HomeViewModel.HomeViewState.GetSSGItemList -> {
-                ssgAdapter.addAll(viewState.list)
+            is HomeViewModel.HomeViewState.RouteContent -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container_route, SSGContentFragment()).commit()
             }
 
-            is HomeViewModel.HomeViewState.Error -> {
+            is HomeViewModel.HomeViewState.RouteDetail -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(
+                        R.id.container_route,
+                        SSGDetailFragment.newInstance(viewState.item)
+                    ).commit()
+            }
 
+            is HomeViewModel.HomeViewState.RouteCurrent -> {
+                supportFragmentManager.beginTransaction()
+                    .setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right)
+                    .replace(
+                        R.id.container_route,
+                        SSGCurrentFragment()
+                    ).commit()
+
+            }
+
+            is HomeViewModel.HomeViewState.ShowProgress -> {
+                with(binding.progressbar) {
+                    bringToFront()
+                    isVisible = true
+                }
+            }
+
+            is HomeViewModel.HomeViewState.HideProgress -> {
+                binding.progressbar.isVisible = false
             }
         }
     }
--- app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/toyssg/viewmodel/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.example.toyssg.viewmodel
 
 import android.app.Application
 import com.example.toyssg.api.response.SSGData
+import com.example.toyssg.api.response.SSGItem
 import com.example.toyssg.base.BaseViewModel
 import com.example.toyssg.base.ViewState
 import com.example.toyssg.data.repo.SSGRepository
@@ -18,23 +19,51 @@ class HomeViewModel @Inject constructor(
 
     fun getSSGItemResponse() {
         ioScope.launch {
+            showProgress()
             when (val result = ssgRepository.getSSGItemResponse()) {
-
                 is Result.Success -> {
                     val resultResponse = result.data
                     viewStateChanged(HomeViewState.GetSSGItemList(resultResponse.dataList))
+                    hideProgress()
                 }
                 is Result.Error -> {
                     viewStateChanged(HomeViewState.Error(result.exception.message.toString()))
+                    hideProgress()
                 }
             }
         }
     }
 
 
+    fun routeContent() {
+        viewStateChanged(HomeViewState.RouteContent)
+    }
+
+    fun routeDetail(item: SSGItem) {
+        viewStateChanged(HomeViewState.RouteDetail(item = item))
+    }
+
+    fun routeCurrent() {
+        viewStateChanged(HomeViewState.RouteCurrent)
+    }
+
+    private fun showProgress() {
+        viewStateChanged(HomeViewState.ShowProgress)
+    }
+
+    private fun hideProgress() {
+        viewStateChanged(HomeViewState.HideProgress)
+    }
+
+
     sealed class HomeViewState : ViewState {
         data class GetSSGItemList(val list: List<SSGData>) : HomeViewState()
         data class Error(val message: String) : HomeViewState()
+        data class RouteDetail(val item: SSGItem) : HomeViewState()
+        object RouteContent : HomeViewState()
+        object RouteCurrent : HomeViewState()
+        object ShowProgress : HomeViewState()
+        object HideProgress : HomeViewState()
     }
 
 }
