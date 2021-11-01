c0b6e4fbd400014de82861cf4d495c7659208455
Parkduksung <duksung1234@naver.com>
2021-11-01 19:41:17 +0900

    [feature] Green, 테스트 코드 통과.

--- app/src/main/java/com/example/toyssg/data/repo/SSGRepository.kt
+++ app/src/main/java/com/example/toyssg/data/repo/SSGRepository.kt
@@ -1,9 +1,18 @@
 package com.example.toyssg.data.repo
 
 import com.example.toyssg.api.response.SSGItemResponse
+import com.example.toyssg.room.SSGEntity
 import com.example.toyssg.util.Result
 
 interface SSGRepository {
 
     suspend fun getSSGItemResponse(): Result<SSGItemResponse>
+
+    suspend fun registerSSGEntity(entity: SSGEntity): Boolean
+
+    suspend fun isExistSSGEntityList(): Boolean
+
+    suspend fun getAllSSGEntity(): Result<List<SSGEntity>>
+
+    suspend fun deleteSSGEntity(entity: SSGEntity) : Boolean
 }
--- app/src/main/java/com/example/toyssg/data/repo/SSGRepositoryImpl.kt
+++ app/src/main/java/com/example/toyssg/data/repo/SSGRepositoryImpl.kt
@@ -1,15 +1,40 @@
 package com.example.toyssg.data.repo
 
 import com.example.toyssg.api.response.SSGItemResponse
+import com.example.toyssg.data.source.local.SSGLocalDataSource
 import com.example.toyssg.data.source.remote.SSGRemoteDataSource
+import com.example.toyssg.room.SSGEntity
 import com.example.toyssg.util.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class SSGRepositoryImpl @Inject constructor(private val ssgRemoteDataSource: SSGRemoteDataSource) : SSGRepository {
+class SSGRepositoryImpl @Inject constructor(
+    private val ssgRemoteDataSource: SSGRemoteDataSource,
+    private val ssgLocalDataSource: SSGLocalDataSource
+) :
+    SSGRepository {
     override suspend fun getSSGItemResponse(): Result<SSGItemResponse> =
         withContext(Dispatchers.IO) {
             return@withContext ssgRemoteDataSource.getSSGItemResponse()
+        }
+
+    override suspend fun registerSSGEntity(entity: SSGEntity): Boolean =
+        withContext(Dispatchers.IO) {
+            return@withContext ssgLocalDataSource.registerSSGEntity(entity)
+        }
+
+    override suspend fun isExistSSGEntityList(): Boolean = withContext(Dispatchers.IO) {
+        return@withContext ssgLocalDataSource.isExistSSGEntityList()
+    }
+
+    override suspend fun getAllSSGEntity(): Result<List<SSGEntity>> =
+        withContext(Dispatchers.IO) {
+            return@withContext ssgLocalDataSource.getAllSSGEntity()
+        }
+
+    override suspend fun deleteSSGEntity(entity: SSGEntity): Boolean =
+        withContext(Dispatchers.IO) {
+            return@withContext ssgLocalDataSource.deleteSSGEntity(entity)
         }
 }
