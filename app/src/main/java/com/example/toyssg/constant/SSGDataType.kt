5849e595eadc832728e09e8e2d06d7e031f05632
Parkduksung <duksung1234@naver.com>
2021-11-01 13:49:15 +0900

    [feature] SSGDataType, Ext 생성.

---
+++ app/src/main/java/com/example/toyssg/constant/SSGDataType.kt
@@ -0,0 +1,5 @@
+package com.example.toyssg.constant
+
+enum class SSGDataTYpe(type: Int) {
+    TYPE_IMAGE(0), TYPE_CURRENT(1) ,TYPE_PRODUCT_ITEM(2)
+}
---
+++ app/src/main/java/com/example/toyssg/ext/SSGDataExt.kt
@@ -0,0 +1,12 @@
+package com.example.toyssg.ext
+
+import com.example.toyssg.api.response.SSGData
+import com.example.toyssg.constant.SSGDataTYpe
+
+fun SSGData.transViewTypeToInt(): Int {
+    return when (viewType) {
+        "image" ->  SSGDataTYpe.TYPE_IMAGE.ordinal
+        "current" ->  SSGDataTYpe.TYPE_CURRENT.ordinal
+        else -> SSGDataTYpe.TYPE_PRODUCT_ITEM.ordinal
+    }
+}
