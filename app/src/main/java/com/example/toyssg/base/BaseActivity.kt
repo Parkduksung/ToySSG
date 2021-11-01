02b548803620d7ea4ff838419793012423402f67
Parkduksung <duksung1234@naver.com>
2021-11-01 18:13:29 +0900

    [feature] BaseActivity 수정.

--- app/src/main/java/com/example/toyssg/base/BaseActivity.kt
+++ app/src/main/java/com/example/toyssg/base/BaseActivity.kt
@@ -1,20 +1,77 @@
 package com.example.toyssg.base
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
 import androidx.annotation.LayoutRes
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.google.android.material.appbar.AppBarLayout
 
 abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
-    AppCompatActivity() {
+    AppCompatActivity(), ToolbarProvider {
 
     protected lateinit var binding: B
 
+    private var toolbarHelper: ToolbarHelper? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         binding = DataBindingUtil.setContentView(this, layoutId)
         setContentView(binding.root)
+        initToolbarHelper()
     }
 
+    private fun initToolbarHelper() {
+        val appBarLayout =
+            (binding.root as ViewGroup).children.find { it is AppBarLayout } as? AppBarLayout
+
+        if (appBarLayout != null) {
+            toolbarHelper = ToolbarHelper(this, appBarLayout).apply {
+                setToolbarTitle(EMPTY_TOOLBAR_TITLE)
+            }
+        }
+    }
+
+    override fun setToolbarTitle(@StringRes titleResId: Int) {
+        toolbarHelper?.setToolbarTitle(titleResId)
+    }
+
+    override fun setToolbarTitle(title: String?) {
+        toolbarHelper?.setToolbarTitle(title)
+    }
+
+    override fun setNavigationIcon(@DrawableRes iconResId: Int) {
+        toolbarHelper?.setNavigationIcon(iconResId)
+    }
+
+    override fun setCustomView(view: View) {
+        toolbarHelper?.setCustomView(view)
+    }
+
+    override fun setCustomView(view: View, relativeHeight: Boolean) {
+        toolbarHelper?.setCustomView(view, relativeHeight)
+    }
+
+    override fun getCustomView(): View? {
+        return toolbarHelper?.getCustomView()
+    }
+
+    override fun setToolbarVisibility(isVisible: Boolean) {
+        toolbarHelper?.setToolbarVisibility(isVisible)
+    }
+
+    override fun setNavigationVisibility(isVisible: Boolean) {
+        toolbarHelper?.setNavigationVisibility(isVisible)
+    }
+
+    companion object {
+        private const val EMPTY_TOOLBAR_TITLE = ""
+    }
 }
